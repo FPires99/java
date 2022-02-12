f172a10b7f785fe9240e1762226d9dfc7dbda27e
FPires99 <91841499+FPires99@users.noreply.github.com>
2022-02-12 17:16:01 +0000

    3rd change

--- HelloWorld/src/Driver.java
+++ HelloWorld/src/Driver.java
@@ -5,16 +5,7 @@ public class Driver
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		int x, y ;
-		x = 30;
-		y = 20;
 		
-		x= x + y;
-		System.out.println(x);
-		
-		if (x>10) {
-			System.out.println("Congratz");
-		}
 	}
 
 }
