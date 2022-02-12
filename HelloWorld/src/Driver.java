7c9223a3fdfccd1ef7c688f0a8d23c482cc8ee49
Babi <Babi@DESKTOP-PP29H6S>
2022-02-12 16:59:28 +0000

    First Change

--- HelloWorld/src/Driver.java
+++ HelloWorld/src/Driver.java
@@ -5,7 +5,12 @@ public class Driver
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-
+		int x, y ;
+		x = 30;
+		y = 20;
+		
+		x= x + y;
+		System.out.println(x);
 	}
 
 }
